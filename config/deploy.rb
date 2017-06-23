1bab761c4719a5535f0e0adbc3a1ebbe31c18230
David Liana <dliana@frontiernet.net>
2017-06-22 20:49:10 -0400

    remove capistrano rule for running paperclip

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,18 +41,3 @@ namespace :deploy do
     end
   end
 end
-
-namespace :paperclip do
-  desc "build missing paperclip styles"
-  task :build_missing_styles do
-    on roles(:app) do
-      within release_path do
-        with rails_env: fetch(:rails_env) do
-          execute :rake, "paperclip:refresh:missing_styles"
-        end
-      end
-    end
-  end
-end
-
-after("deploy:compile_assets", "paperclip:build_missing_styles")
